ee321577001752d8284decd25b1f747391880abb
amitdubey4 <142138081+amitdubey4@users.noreply.github.com>
2024-05-20 11:05:10 +0530

    Update application.py

--- application.py
+++ application.py
@@ -4,7 +4,8 @@ import pickle
 import pandas as pd
 import numpy as np
 
-app=Flask(__name__)
+application = Flask(__name__)
+app = application
 cors=CORS(app)
 model=pickle.load(open('LinearRegressionModel.pkl','rb'))
 car=pd.read_csv('Cleaned_Car_data.csv')
